9c12cca4660c58b50a5e6e6d486ee339f1b026e4
GutoAS <agostinhosoberano2@gmail.com>
2023-01-06 14:11:01 +0200

    update card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,20 @@ import CardCarousel from "./CartCarousel";
 const Card = () => {
   return (
     <section id="card" className="my-5">
-      <div className="text-center mx-auto w-50">
-        <h4 className="fw-bold">
-          Most <span> popular affordable pricing </span> per
+      <div className="text-center mx-auto w-50 d-none d-md-block">
+        <h4 className="fw-bold ">
+          Most{" "}
+          <span
+            style={{
+              borderBottomWidth: "6px",
+              borderBottomStyle: "solid",
+              borderBottomColor: "rgb(255, 187, 0)",
+            }}
+          >
+            {" "}
+            popular affordable pricing{" "}
+          </span>{" "}
+          per
           <br />
           jurisdictions are brought to you to <br />
           kick off your adventure.
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -11,24 +11,28 @@ const CardItem = (props) => {
     } else {
       setStyle("");
     }
-  }, [isHovered])
+  }, [isHovered]);
 
   return (
-    <div className={"card-item " + style} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-      <h5 className="card-item-title">
-        {props.title}
-      </h5>
+    <div
+      className={"card-item " + style}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <h5 className="card-item-title">{props.title}</h5>
       <div className="card-price">
         <span className="card-price-left">{props.price}</span>
         <span className="card-price-right"> One Time Payment</span>
       </div>
       <div className="card-include ">
-        <div className="card-include-title">{props.title} New Company formation includes</div>
+        <div className="card-include-title">
+          {props.title} New Company formation includes
+        </div>
         <CardIncludeList />
       </div>
       <button className="cart-button">Continue</button>
     </div>
-  )
-}
+  );
+};
 
-export default CardItem
+export default CardItem;
